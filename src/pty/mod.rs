8380308c874c3ff9a4f9a4b7a79af0f325859023
obsidian <artem-bambalov@yandex.ru>
2026-01-25 23:53:40 +0200

    feat: handle PTY resize events

--- src/pty/mod.rs
+++ src/pty/mod.rs
@@ -1,21 +1,32 @@
 pub mod vt;
 
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode, size as terminal_size};
-use portable_pty::{native_pty_system, CommandBuilder, PtySize};
+use portable_pty::{native_pty_system, CommandBuilder, MasterPty, PtySize};
 use std::error::Error;
 use std::io::{self, Read, Write};
+use std::sync::{Arc, Mutex};
 use std::thread;
 use termwiz::escape::Action;
+use termwiz::surface::Surface;
 use vt::VtParser;
+
+#[cfg(unix)]
+use signal_hook::consts::signal::SIGWINCH;
+#[cfg(unix)]
+use signal_hook::iterator::Signals;
 
 pub struct PtyManager {
     vt_parser: VtParser,
+    screen: Arc<Mutex<Surface>>,
 }
 
 impl PtyManager {
     pub fn new() -> Self {
+        let (cols, rows) = terminal_size().unwrap_or((80, 24));
+        let screen = Surface::new(usize::from(cols), usize::from(rows));
         Self {
             vt_parser: VtParser::new(),
+            screen: Arc::new(Mutex::new(screen)),
         }
     }
 
@@ -36,6 +47,7 @@ impl PtyManager {
             pixel_width: 0,
             pixel_height: 0,
         })?;
+        self.resize_screen(cols, rows);
 
         let mut cmd = CommandBuilder::new(command);
         cmd.args(args);
@@ -46,8 +58,12 @@ impl PtyManager {
 
         let raw_mode_guard = RawModeGuard::new()?;
 
-        let reader = pair.master.try_clone_reader()?;
-        let writer = pair.master.take_writer()?;
+        let master = pair.master;
+        let reader = master.try_clone_reader()?;
+        let writer = master.take_writer()?;
+        let resize_master = Arc::new(Mutex::new(master));
+        let resize_watcher =
+            ResizeWatcher::start(Arc::clone(&resize_master), Arc::clone(&self.screen))?;
 
         let reader_handle = thread::spawn(move || {
             let mut reader = reader;
@@ -91,6 +107,9 @@ impl PtyManager {
 
         let status = child.wait()?;
         drop(raw_mode_guard);
+        if let Some(watcher) = resize_watcher {
+            watcher.stop();
+        }
         let _ = reader_handle.join();
 
         if status.success() {
@@ -98,6 +117,12 @@ impl PtyManager {
         }
 
         std::process::exit(status.exit_code() as i32);
+    }
+
+    fn resize_screen(&self, cols: u16, rows: u16) {
+        if let Ok(mut screen) = self.screen.lock() {
+            screen.resize(usize::from(cols), usize::from(rows));
+        }
     }
 }
 
@@ -127,6 +152,62 @@ impl RawModeGuard {
 impl Drop for RawModeGuard {
     fn drop(&mut self) {
         let _ = disable_raw_mode();
+    }
+}
+
+struct ResizeWatcher {
+    #[cfg(unix)]
+    handle: signal_hook::iterator::Handle,
+    #[cfg(unix)]
+    thread: thread::JoinHandle<()>,
+}
+
+impl ResizeWatcher {
+    fn start(
+        master: Arc<Mutex<Box<dyn MasterPty + Send>>>,
+        screen: Arc<Mutex<Surface>>,
+    ) -> Result<Option<Self>, Box<dyn Error>> {
+        #[cfg(unix)]
+        {
+            let mut signals = Signals::new([SIGWINCH])?;
+            let handle = signals.handle();
+            let thread = thread::spawn(move || {
+                for _ in signals.forever() {
+                    let (cols, rows) = match terminal_size() {
+                        Ok(size) => size,
+                        Err(_) => continue,
+                    };
+                    let size = PtySize {
+                        rows,
+                        cols,
+                        pixel_width: 0,
+                        pixel_height: 0,
+                    };
+                    if let Ok(master) = master.lock() {
+                        let _ = master.resize(size);
+                    }
+                    if let Ok(mut screen) = screen.lock() {
+                        screen.resize(usize::from(cols), usize::from(rows));
+                    }
+                }
+            });
+            return Ok(Some(Self { handle, thread }));
+        }
+
+        #[cfg(not(unix))]
+        {
+            let _ = master;
+            let _ = screen;
+            Ok(None)
+        }
+    }
+
+    fn stop(self) {
+        #[cfg(unix)]
+        {
+            self.handle.close();
+            let _ = self.thread.join();
+        }
     }
 }
 
